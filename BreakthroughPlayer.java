3c107f95d6ec555adeaafeb9052f7c0b1485daca
Justin Ordway <ordwayjm@miamioh.edu>
2013-10-17 16:11:08 -0400

    random test player

--- BreakthroughPlayer.java
+++ BreakthroughPlayer.java
@@ -1,19 +1,45 @@
-package breakthrough;
+package minmaxPlayer;
+
 import game.*;
 import java.util.*;
+import breakthrough.*;
 
 public class BreakthroughPlayer extends GamePlayer {
-	public BreakthroughPlayer(String n) 
-	{
+	
+	public BreakthroughPlayer(String n) {
 		super(n, new BreakthroughState(), false);
 	}
-	public GameMove getMove(GameState state, String lastMove)
-	{
-		
+	
+	public GameMove getMove(GameState state, String lastMove) {
+		BreakthroughState board = (BreakthroughState)state;
+		ArrayList<BreakthroughMove> list = new ArrayList<BreakthroughMove>();  
+		BreakthroughMove mv = new BreakthroughMove();
+		int dir = state.who == GameState.Who.HOME ? +1 : -1;
+		for (int r=0; r<BreakthroughState.N; r++) {
+			for (int c=0; c<BreakthroughState.N; c++) {
+				mv.startRow = r;
+				mv.startCol = c;
+				mv.endingRow = r+dir; mv.endingCol = c;
+				if (board.moveOK(mv)) {
+					list.add((BreakthroughMove)mv.clone());
+				}
+				mv.endingRow = r+dir; mv.endingCol = c+1;
+				if (board.moveOK(mv)) {
+					list.add((BreakthroughMove)mv.clone());
+				}
+				mv.endingRow = r+dir; mv.endingCol = c-1;
+				if (board.moveOK(mv)) {
+					list.add((BreakthroughMove)mv.clone());
+				}
+			}
+		}
+		int which = Util.randInt(0, list.size()-1);
+		return list.get(which);
 	}
-	public static void main(String [] args)
-	{
-		GamePlayer p = new BreakthroughPlayer("Random BT+");
+	
+	public static void main(String [] args) {
+		GamePlayer p = new RandomBreakthroughPlayer("Test Breakthrough");
 		p.compete(args);
 	}
 }
+
