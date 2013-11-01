5cfb2e482de385ea266f32d8416b31f1f5a8de03
Justin Ordway <ordwayjm@miamioh.edu>
2013-11-01 17:29:48 -0400

    commit

--- BreakthroughPlayer.java
+++ BreakthroughPlayer.java
@@ -8,23 +8,22 @@ import breakthrough.*;
 public class BreakthroughPlayer extends BaseBreakthroughPlayer {
 
 	protected ScoredBreakthroughMove[] mvStack;
-	public final int DEPTH_LIMIT = 5;
+	public final int DEPTH_LIMIT = 15;
 
 	protected class ScoredBreakthroughMove extends BreakthroughMove {
-		public ScoredBreakthroughMove(int r1, int c1, int r2, int c2, double s)
-		{
+		public ScoredBreakthroughMove(int r1, int c1, int r2, int c2, double s) {
 			super(r1, c1, r2, c2);
 			score = s;
 		}
 
-		public void set(int r1, int c1, int r2, int c2, double s)
-		{
+		public void set(int r1, int c1, int r2, int c2, double s) {
 			startRow = r1;
 			startCol = c1;
 			endingRow = r2;
 			endingCol = c2;
 			score = s;
 		}
+		
 		public double score;
 	}
 
@@ -101,7 +100,7 @@ public class BreakthroughPlayer extends BaseBreakthroughPlayer {
 		}
 		ScoredBreakthroughMove bestMove = mvStack[depth];
 		if(toMaximize) {
-			bestValue = Double.NEGATIVE_INFINITY;
+			bestValue = Double.POSITIVE_INFINITY;
 			bestMove.set(0,0,0,0,bestValue);
 			for(BreakthroughMove mv : getMoves(board, BreakthroughState.homeSym)) {
 				BreakthroughState temp = makeMove(board, mv);
@@ -111,7 +110,7 @@ public class BreakthroughPlayer extends BaseBreakthroughPlayer {
 				}
 			}
 		} else {
-			bestValue = Double.POSITIVE_INFINITY;
+			bestValue = Double.NEGATIVE_INFINITY;
 			bestMove.set(0,0,0,0, bestValue);
 			for(BreakthroughMove mv : getMoves(board, BreakthroughState.awaySym)) {
 				BreakthroughState temp = makeMove(board, mv);
